67081bfa6430ead24bae330526374b8e64a0d3c4
Liv Sarah <h182021@stud.hib.no>
2017-10-23 19:13:20 +0000

    Definert UrlMappings under controller
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@57 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/controller/UrlMappings.java
+++ src/no/hvl/dat104/controller/UrlMappings.java
@@ -4,9 +4,20 @@ package no.hvl.dat104.controller;
  * Created by Peder on 27.09.2017.
  */
 public class UrlMappings {
-	public static final String LOGIN_URL = "logginn";
-    public static final String LOGOUT_URL = "logout";
-    public static final String REGISTRER_URL = "registrer";
-    public static final String LANDING_URL = "";
-    public static final String SETTINGS_URL = "settings";
+	public static final String ADMINISTRER_URL = "administrer";
+    public static final String GODKJENNBRUKER_URL = "godkjennbruker";
+    public static final String DELTAEVENT_URL = "deltaevent";
+    public static final String EVENTFERDIG_URL = "eventferdig";
+    public static final String GITILBAKEMELDING_URL = "gitilbakemelding";
+	public static final String EVENTRESULTATER_URL = "eventresultater";
+    public static final String LAGAKTIVITET_URL = "lagaktivitet";
+    public static final String LAGEVENT_URL = "lagevent";
+    public static final String MINEAKTIVITETER_URL = "mineaktiviteter";
+    public static final String OPPRETTBRUKER_URL = "opprettbruker";
+	public static final String REDIGERAKTIVITET_URL = "redigeraktivitet";
+    public static final String REDIGEREVENT_URL = "redigerevent";
+    public static final String VISAKTIVITET_URL = "visaktivitet";
+    public static final String LANDING_URL = "landing";
+    public static final String LOGGINN_URL = "logginn";
+    
 }
