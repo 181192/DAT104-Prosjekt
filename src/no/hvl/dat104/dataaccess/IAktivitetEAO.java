fa769aca842f95b0be46c06841fdb5a92b792f2d
Liv Sarah <h182021@stud.hib.no>
2017-10-23 18:18:50 +0000

    Definerte IAktivitetEAO og AktivitetEAO
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@42 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/dataaccess/IAktivitetEAO.java
+++ src/no/hvl/dat104/dataaccess/IAktivitetEAO.java
@@ -1,5 +1,49 @@
 package no.hvl.dat104.dataaccess;
 
+import java.util.List;
+
+import no.hvl.dat104.model.Aktivitet;
+
 public interface IAktivitetEAO {
 
+	/**
+	 * Legger til en aktivitet
+	 * 
+	 * @param a
+	 *		Aktivitet
+	 */
+	public void leggTilAktivitet(Aktivitet a);
+
+	/**
+	 * Finner en aktivitet
+	 * 
+	 * @param id
+	 *		Id til aktiviteten
+	 */
+	public Aktivitet finnAktivitet(Integer id);
+	
+	/**
+	 * Oppdaterer en aktivitet
+	 * 
+	 * @param a
+	 *		Aktivitet
+	 */
+	public void oppdaterAktivitet(Aktivitet a);
+	
+	/**
+	 * Sletter en aktivitet
+	 * 
+	 * @param a
+	 *		Aktivitet
+	 */
+	public void slettAktivitet(Aktivitet a);
+	
+	/**
+	 * Returnerer en liste med alle aktiviteter
+	 *
+	 * @return Alle deltakere
+	 */
+	public List<Aktivitet> alleAktiviteter();
+	
+	
 }
--- src/no/hvl/dat104/dataaccess/jpa/AktivitetEAO.java
+++ src/no/hvl/dat104/dataaccess/jpa/AktivitetEAO.java
@@ -1,7 +1,40 @@
 package no.hvl.dat104.dataaccess.jpa;
 
+import java.util.List;
+
 import no.hvl.dat104.dataaccess.IAktivitetEAO;
+import no.hvl.dat104.model.Aktivitet;
 
 public class AktivitetEAO implements IAktivitetEAO {
+
+	@Override
+	public void leggTilAktivitet(Aktivitet a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Aktivitet finnAktivitet(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void oppdaterAktivitet(Aktivitet a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void slettAktivitet(Aktivitet a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Aktivitet> alleAktiviteter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
