85a433df07d6802ab49f59cac3c928dd4d0b960f
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-10-23 18:31:09 +0000

    Definert ITilbakemeldingEAO og TilbakemeldingEAO
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@48 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/dataaccess/ITilbakemeldingEAO.java
+++ src/no/hvl/dat104/dataaccess/ITilbakemeldingEAO.java
@@ -1,5 +1,47 @@
 package no.hvl.dat104.dataaccess;
 
+import java.util.List;
+
+import no.hvl.dat104.model.Tilbakemelding;
+
 public interface ITilbakemeldingEAO {
+	/**
+	 * Legger til en tilbakemelding
+	 * 
+	 * @param t
+	 *            Tilbakemelding
+	 */
+	public void leggTilTilbakemelding(Tilbakemelding t);
+
+	/**
+	 * Finner en tilbakemelding
+	 * 
+	 * @param id
+	 *            Id til tilbakemelding
+	 */
+	public Tilbakemelding finnTilbakemelding(Integer id);
+
+	/**
+	 * Oppdaterer en tilbakemelding
+	 * 
+	 * @param t
+	 *            Tilbakemelding
+	 */
+	public void oppdaterTilbakemelding(Tilbakemelding t);
+
+	/**
+	 * Sletter en tilbakemelding
+	 * 
+	 * @param t
+	 *            Tilbakemelding
+	 */
+	public void slettTilbakemelding(Tilbakemelding t);
+
+	/**
+	 * Returnerer en liste med alle tilbakemeldingene
+	 *
+	 * @return Alle tilbakemeldingene
+	 */
+	public List<Tilbakemelding> alleTilbakemeldingene();
 
 }
--- src/no/hvl/dat104/dataaccess/jpa/TilbakemeldingEAO.java
+++ src/no/hvl/dat104/dataaccess/jpa/TilbakemeldingEAO.java
@@ -1,7 +1,40 @@
 package no.hvl.dat104.dataaccess.jpa;
 
+import java.util.List;
+
 import no.hvl.dat104.dataaccess.ITilbakemeldingEAO;
+import no.hvl.dat104.model.Tilbakemelding;
 
 public class TilbakemeldingEAO implements ITilbakemeldingEAO {
+
+	@Override
+	public void leggTilTilbakemelding(Tilbakemelding t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Tilbakemelding finnTilbakemelding(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void oppdaterTilbakemelding(Tilbakemelding t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void slettTilbakemelding(Tilbakemelding t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Tilbakemelding> alleTilbakemeldingene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
