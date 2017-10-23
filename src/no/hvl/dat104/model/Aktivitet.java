3ebbe91ac20b815cce9892932d4754d5cd40497a
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-10-23 17:24:29 +0000

    La til aktivitet
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@34 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/model/Aktivitet.java
+++ src/no/hvl/dat104/model/Aktivitet.java
@@ -1,6 +1,96 @@
 package no.hvl.dat104.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "Aktivitet")
+@Table(name = "Aktivitet", schema = "db")
 public class Aktivitet {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String navn;
+	private String status;
+	@Column(name = "id_Bruker")
+	private Integer idBruker;
+
+	/**
+	 * Tom kontruktor for Aktivitet
+	 */
+	public Aktivitet() {
+		this("", "", null);
+	}
+
+	/**
+	 * Kontruktor for Aktivitet
+	 * 
+	 * @param navn
+	 *            Navnet på aktivitet
+	 * @param status
+	 *            Statusen på aktiviteten
+	 * @param idBruker
+	 *            Id til bruker som opprettet aktiviteten
+	 */
+	public Aktivitet(String navn, String status, Integer idBruker) {
+		this.navn = navn;
+		this.status = status;
+		this.idBruker = idBruker;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @return the navn
+	 */
+	public String getNavn() {
+		return navn;
+	}
+
+	/**
+	 * @param navn
+	 *            the navn to set
+	 */
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status
+	 *            the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the idBruker
+	 */
+	public Integer getIdBruker() {
+		return idBruker;
+	}
+
+	/**
+	 * @param idBruker
+	 *            the idBruker to set
+	 */
+	public void setIdBruker(Integer idBruker) {
+		this.idBruker = idBruker;
+	}
 
 }
