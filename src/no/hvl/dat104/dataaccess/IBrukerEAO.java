f3b70827f80d1ccdae7b5f21773a3b03d08260a8
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-10-23 18:19:40 +0000

    Definert IBrukerEAO og BrukerEAO
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@43 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/dataaccess/IBrukerEAO.java
+++ src/no/hvl/dat104/dataaccess/IBrukerEAO.java
@@ -1,5 +1,49 @@
 package no.hvl.dat104.dataaccess;
 
+import java.util.List;
+
+import no.hvl.dat104.model.Bruker;
+
 public interface IBrukerEAO {
+
+	/**
+	 * Legger til en bruker
+	 * 
+	 * @param b
+	 *            Brukeren som skal bli lagt til
+	 */
+	public void leggTilBruker(Bruker b);
+
+	/**
+	 * Finner en bruker basert på id
+	 * 
+	 * @param id
+	 *            Id til brukeren
+	 * @return En bruker
+	 */
+	public Bruker finnBruker(Integer id);
+
+	/**
+	 * Oppdaterer brukeren
+	 * 
+	 * @param b
+	 *            Brukeren som skal oppdateres
+	 */
+	public void oppdaterBruker(Bruker b);
+
+	/**
+	 * Sletter en bruker
+	 * 
+	 * @param b
+	 *            Brukeren som skal slettes
+	 */
+	public void slettBruker(Bruker b);
+
+	/**
+	 * Returnerer en liste med alle brukerne
+	 * 
+	 * @return Alle brukerne
+	 */
+	public List<Bruker> alleBrukerne();
 
 }
--- src/no/hvl/dat104/dataaccess/jpa/BrukerEAO.java
+++ src/no/hvl/dat104/dataaccess/jpa/BrukerEAO.java
@@ -1,10 +1,40 @@
 package no.hvl.dat104.dataaccess.jpa;
 
-import no.hvl.dat104.dataaccess.IBrukerEAO;
+import java.util.List;
 
-//@Stateless
-public class BrukerEAO implements IBrukerEAO{
-//	@PersistenceContext(name = "g03")
-//    private EntityManager em;
+import no.hvl.dat104.dataaccess.IBrukerEAO;
+import no.hvl.dat104.model.Bruker;
+
+public class BrukerEAO implements IBrukerEAO {
+
+	@Override
+	public void leggTilBruker(Bruker b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Bruker finnBruker(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void oppdaterBruker(Bruker b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void slettBruker(Bruker b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Bruker> alleBrukerne() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
