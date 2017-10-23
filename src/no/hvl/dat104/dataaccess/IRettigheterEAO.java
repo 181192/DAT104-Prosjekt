954baff17c19d6a4b86f9c61d217b5a848494733
Liv Sarah <h182021@stud.hib.no>
2017-10-23 18:30:08 +0000

    Definert IRettigheterEAO og RettigheterEAO i dataaccess
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@47 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/dataaccess/IRettigheterEAO.java
+++ src/no/hvl/dat104/dataaccess/IRettigheterEAO.java
@@ -1,5 +1,49 @@
 package no.hvl.dat104.dataaccess;
 
+import java.util.List;
+
+import no.hvl.dat104.model.Rettigheter;
+
 public interface IRettigheterEAO {
 
+	/**
+	 * Legger til en Rettigheter
+	 * 
+	 * @param r
+	 *		Rettigheter
+	 */
+	public void leggTilRettigheter(Rettigheter r);
+
+	/**
+	 * Finner en rettighet
+	 * 
+	 * @param id
+	 *		Id til rettigheten
+	 */
+	public Rettigheter finnRettigheter(Integer id);
+	
+	/**
+	 * Oppdaterer en rettigheter
+	 * 
+	 * @param r
+	 *		Rettigheter
+	 */
+	public void oppdaterRettigheter(Rettigheter r);
+	
+	/**
+	 * Sletter en rettigheter
+	 * 
+	 * @param r
+	 *		Rettigheter
+	 */
+	public void slettRettigheter(Rettigheter r);
+	
+	/**
+	 * Returnerer en liste med alle rettigheter
+	 *
+	 * @return Alle rettigheter
+	 */
+	public List<Rettigheter> alleRettigheter();
+	
+	
 }
--- src/no/hvl/dat104/dataaccess/jpa/RettigheterEAO.java
+++ src/no/hvl/dat104/dataaccess/jpa/RettigheterEAO.java
@@ -1,7 +1,41 @@
 package no.hvl.dat104.dataaccess.jpa;
 
+import java.util.List;
+
 import no.hvl.dat104.dataaccess.IRettigheterEAO;
+import no.hvl.dat104.model.Rettigheter;
 
 public class RettigheterEAO implements IRettigheterEAO {
+
+	
+	@Override
+	public void leggTilRettigheter(Rettigheter r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Rettigheter finnRettigheter(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void oppdaterRettigheter(Rettigheter r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void slettRettigheter(Rettigheter r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Rettigheter> alleRettigheter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
