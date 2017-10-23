4496d1d1d8e37df5180986c7a9bcc3939161f713
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-10-23 18:27:50 +0000

    Definert IRolleEAO og RolleEAO
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@46 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/dataaccess/IRolleEAO.java
+++ src/no/hvl/dat104/dataaccess/IRolleEAO.java
@@ -1,5 +1,44 @@
 package no.hvl.dat104.dataaccess;
 
-public interface IRolleEAO {
+import java.util.List;
 
+import no.hvl.dat104.model.Rolle;public interface IRolleEAO {
+	/**
+	 * Legger til en rolle
+	 * 
+	 * @param r
+	 *            Rolle
+	 */
+	public void leggTilRolle(Rolle r);
+
+	/**
+	 * Finner en rolle
+	 * 
+	 * @param id
+	 *            Id til rolle
+	 */
+	public Rolle finnRolle(Integer id);
+
+	/**
+	 * Oppdaterer en rolle
+	 * 
+	 * @param r
+	 *            Rolle
+	 */
+	public void oppdaterRolle(Rolle r);
+
+	/**
+	 * Sletter en rolle
+	 * 
+	 * @param r
+	 *            Rolle
+	 */
+	public void slettRolle(Rolle r);
+
+	/**
+	 * Returnerer en liste med alle rollene
+	 *
+	 * @return Alle rollene
+	 */
+	public List<Rolle> alleRollene();
 }
--- src/no/hvl/dat104/dataaccess/jpa/RolleEAO.java
+++ src/no/hvl/dat104/dataaccess/jpa/RolleEAO.java
@@ -1,7 +1,40 @@
 package no.hvl.dat104.dataaccess.jpa;
 
+import java.util.List;
+
 import no.hvl.dat104.dataaccess.IRolleEAO;
+import no.hvl.dat104.model.Rolle;
 
 public class RolleEAO implements IRolleEAO {
+
+	@Override
+	public void leggTilRolle(Rolle r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Rolle finnRolle(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void oppdaterRolle(Rolle r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void slettRolle(Rolle r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Rolle> alleRollene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
