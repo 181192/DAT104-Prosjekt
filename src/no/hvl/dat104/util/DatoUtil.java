c90a2f8d6845feafb698bfa74dedddf3acd9bbf8
Peder Wiig <peder.wiig@gmail.com>
2017-10-25 20:41:04 +0000

    La til javadoc
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@75 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/util/DatoUtil.java
+++ src/no/hvl/dat104/util/DatoUtil.java
@@ -6,16 +6,35 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatoUtil {
+	/**
+	 * Foraterer og konkatinerer to strenger til en timestamp
+	 * @param dato
+	 * @param klokkeslett
+	 * @return sql.Timestamp
+	 * @throws ParseException
+	 */
 	public static Timestamp formaterDatoTilStamp(String dato, String klokkeslett) throws ParseException {
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm");
         java.util.Date startDato= df.parse(dato + " " + klokkeslett);
         java.sql.Timestamp sq = new java.sql.Timestamp(startDato.getTime());
         return sq;
     }
+	/**
+	 * Formaterer en timestamp til en dato.
+	 * @param timestamp
+	 * @return Date Object
+	 */
     public static Date formaterStampTilDato(Timestamp timestamp) {
         Date date = new Date(timestamp.getTime());
         return date;
     }
+    /**
+     * Konkatinerer en dato og et klokkeslett til en streng.
+     * @param dato
+     * @param klokkeslett
+     * @return formatert
+     * @throws ParseException
+     */
     public static String fintFormatertDato(String dato, String klokkeslett) throws ParseException {
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm");
         java.util.Date startDato = df.parse(dato + " " + klokkeslett);
