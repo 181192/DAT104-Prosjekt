f995d7fbbde6eaf8ad0c475ab5deee3adefae012
David <h125370@stud.hib.no>
2017-10-26 21:58:47 +0000

    Lagt til noen private hjelpemetoder som produserer noe "dummydata" og sender til jspen.
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@83 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/controller/styrer/liveevent/LiveEventServlet.java
+++ src/no/hvl/dat104/controller/styrer/liveevent/LiveEventServlet.java
@@ -1,6 +1,11 @@
 package no.hvl.dat104.controller.styrer.liveevent;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,8 +21,15 @@ public class LiveEventServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		request.getRequestDispatcher("WEB-INF/views/styrer/liveevent/liveevent.jsp").forward(request, response);
+		System.out.println("doGet LiveEventServlet kjører.");
+
+		//Lager noen testdata og sender til jsp i requesten. 
+		List<Integer> dummyData = lagDummyListe(50, 20);
+		List<Integer> dummyDataFT = lagFrekvensTabell(dummyData, 60);
+		request.setAttribute("dummyData", dummyDataFT);
+		request.getRequestDispatcher("WEB-INF/views/styrer/liveevent/liveevent.jsp").
+						forward(request, response);
+		
 	}
 
 	/**
@@ -27,5 +39,46 @@ public class LiveEventServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	
+	
+	
+	
+	
+	/**
+	 * Mockup for testdata.
+	 * @param antall
+	 * @param maxTid
+	 * @return
+	 */
+    private List<Integer> lagDummyListe(int antall, int maxTid){
+        List<Integer> tbm = new ArrayList<>();
+        Random r = new Random();
+        for(int i= 0; i < antall; i++){
+            tbm.add(r.nextInt(maxTid));
+        }
+        
+        return tbm;
+    }
+    
+    /**
+     * Mockup for testdata
+     * @param alleTilbakemeldinger
+     * @param maxVerdi
+     * @return
+     */
+    private List<Integer> lagFrekvensTabell(List<Integer> alleTilbakemeldinger, Integer maxVerdi){
+        Integer[] ftb = new Integer[maxVerdi];
+        
+        for(int i = 0; i < maxVerdi; i++){
+            ftb[i] = 0;
+        }
+        
+        for(int i = 0; i < alleTilbakemeldinger.size(); i++){
+            int denne = alleTilbakemeldinger.get(i);
+            ftb[denne] += 1;
+        }
+        
+        return Arrays.asList(ftb);
+    }
 
 }
