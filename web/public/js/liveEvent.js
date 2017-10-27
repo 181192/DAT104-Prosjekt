c1aa4e8dd2066bdbf8c0763912a64a2489a012df
David <h125370@stud.hib.no>
2017-10-27 00:42:22 +0000

    Kan oppdatere grafen med knapper. Faker innput.
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@86 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- web/public/js/liveEvent.js
+++ web/public/js/liveEvent.js
@@ -1,10 +1,50 @@
+'use strict'
+
+/*
+ * frekvensTabell: streng fra servlet. antallSlotter: hvor mange slotter vi skal
+ * ha i diagrammet. slottBredde: hvor mange minutter hver slott skal
+ * representere.
+ */
+
+function vaskData(frekvensTabell, antallSlotter, slottBredde) {
+	let dataArr = lagDataArray(antallSlotter);
+	let frekvensArr = lagFrekvensArray(frekvensTabell);
+
+	for (let i = 0; i < antallSlotter; i++) {
+		let label = "" + (i + 1) * slottBredde + "";
+		let daarligIndex = i * slottBredde;
+		let middelsIndex = i * slottBredde + 1;
+		let godIndex = i * slottBredde + 2;
+		dataArr[i + 1][0] = label;
+		dataArr[i + 1][1] = parseInt(frekvensArr[daarligIndex]);
+		dataArr[i + 1][2] = parseInt(frekvensArr[middelsIndex]);
+		dataArr[i + 1][3] = parseInt(frekvensArr[godIndex]);
+	}
+
+	function lagDataArray(antallSlotter) {
+		// Legger til en ekstra på grunn av "labels"
+		let res = new Array(antallSlotter + 1);
+		res[0] = [ 'Minutter', 'Daarlig', 'Middels', 'Bra' ];
+		for (let i = 1; i < antallSlotter + 1; i++) {
+			res[i] = new Array(4);
+		}
+		return res;
+	}
+
+	function lagFrekvensArray(frekvensTabell) {
+		let fSliced = frekvensTabell.slice(1, -1);
+		return fSliced.split(", ");
+	}
+	return dataArr;
+}
+
 function tegnDiagram() {
 
 	const FARGE_SUR = '#CD3337';
 	const FARGE_MIDDELS = '#CF7C34';
 	const FARGE_GLAD = '#30A82A';
 
-	var dataArray = formatData(frekvensTabell, 15, 4);
+	var dataArray = klargjortData;	//vaskData(frekvensTabell, 15, 4);
 	var button = document.getElementById('start-stopp');
 	var chartDiv = document.getElementById('chart_div');
 	var data = google.visualization.arrayToDataTable(dataArray);
@@ -58,41 +98,21 @@ function tegnDiagram() {
 		startHandeler();
 	}
 
-	/*
-	 * frekvensTabell: streng fra servlet. antallSlotter: hvor mange slotter vi
-	 * skal ha i diagrammet. slottBredde: hvor mange minutter hver slott skal
-	 * representere.
-	 */
-	function formatData(frekvensTabell, antallSlotter, slottBredde) {
-		let dataArr = lagDataArray(antallSlotter);
-		let frekvensArr = lagFrekvensArray(frekvensTabell);
-
-		for (let i = 0; i < antallSlotter; i++) {
-			let label = "" + (i + 1) * slottBredde + "";
-			let daarligIndex = i * slottBredde;
-			let middelsIndex = i * slottBredde + 1;
-			let godIndex = i * slottBredde + 2;
-			dataArr[i + 1][0] = label;
-			dataArr[i + 1][1] = parseInt(frekvensArr[daarligIndex]);
-			dataArr[i + 1][2] = parseInt(frekvensArr[middelsIndex]);
-			dataArr[i + 1][3] = parseInt(frekvensArr[godIndex]);
-		}
-		return dataArr;
-	}
-
-	function lagDataArray(antallSlotter) {
-		// Legger til en ekstra på grunn av "labels"
-		let res = new Array(antallSlotter + 1);
-		res[0] = [ 'Minutter', 'Daarlig', 'Middels', 'Bra' ];
-		for (let i = 1; i < antallSlotter + 1; i++) {
-			res[i] = new Array(4);
-		}
-		return res;
-	}
-	function lagFrekvensArray(frekvensTabell) {
-		let fSliced = frekvensTabell.slice(1, -1);
-		return fSliced.split(", ");
-	}
-
 	drawClassicChart();
 };
+
+function daarligKnappHandeler() {
+	klargjortData[6][1] += 1;
+	tegnDiagram();
+
+}
+
+function middelsKnappHandeler() {
+	klargjortData[6][2] += 1;
+	tegnDiagram();
+}
+
+function godKnappHandeler() {
+	klargjortData[6][3] += 1;
+	tegnDiagram();
+}
