fd48dc541edc3f64f7a95c22f82ea288ea13ecb2
Karl <h181207@stud.hib.no>
2017-10-29 14:28:44 +0000

    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@111 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- test/no/hvl/dat104/controller/deltager/GiTilbakemeldingControllerTest.java
+++ test/no/hvl/dat104/controller/deltager/GiTilbakemeldingControllerTest.java
@@ -2,32 +2,56 @@ package no.hvl.dat104.controller.deltager;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import no.hvl.dat104.controller.deltager.GiTilbakemeldingController;
 
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 
 public class GiTilbakemeldingControllerTest {
-    /*MockHttpServletRequest fakeRequest;
-    MockHttpServletResponse fakeResponse;
+	/*@Mock HttpServletRequest stubRequest;
+	@Mock HttpServletResponse stubResponse;
+	@Mock RequestDispatcher stubRequestDispatcher;
     GiTilbakemeldingController giTilbakemeldingController;
     
 	@Before
-	public void setUp() throws Exception {
-        fakeRequest = new MockHttpServletRequest();
-        fakeResponse = new MockHttpServletResponse();
+	public void setUp() {
         giTilbakemeldingController = new GiTilbakemeldingController();
+        
+        when(stubRequest.getRequestDispatcher(any(String.class))).thenReturn(stubRequestDispatcher);
 	}
 
-	@Test
-	public void tilbakemeldingSkalLiggeISomParameter() {
-        fakeRequest.setParameter("tilbakemelding", "0");
-        //gir feilmelding karl. Og Tester skal ligger under Java Resources/test
-        giTilbakemeldingController.doGet(fakeRequest,fakeResponse);
+    @Test
+    public void atDetGjoresEnForwardTilKorrektJsp() throws ServletException, IOException {
         
-	}*/
+    	giTilbakemeldingController.doGet(stubRequest, stubResponse);
+        
+        verify(stubRequest).getRequestDispatcher("WEB-INF/views/deltager/gitilbakemelding.jsp");
+        verify(stubRequestDispatcher).forward(stubRequest, stubResponse);
+    }*/
+
 
 }
