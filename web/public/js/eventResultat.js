6f6775b66bb7f233818c07a00829f9dd926e4dbf
Fredrik <h148916@stud.hib.no>
2017-10-31 15:43:56 +0000

    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@136 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- web/public/js/eventResultat.js
+++ web/public/js/eventResultat.js
@@ -13,45 +13,27 @@
 
        // Create our data table.
        var array = [];
-       array.push(['Minutter', 'Fornoyd', 'Noytral', 'Misfornoyd']);
+       array.push(['Minutter', 'Fornøyd', 'Nøytral', 'Misfornøyd']);
 
-       for (var i = 1; i < 60; i++) {
+       for (var i = 1; i < 200; i++) {
          array.push([i, Math.floor(Math.random() * 10 + 1), Math.floor(Math.random() * 10 + 1), Math.floor(Math.random() * 10 + 1)]);
        }
 
        var data = google.visualization.arrayToDataTable(array);
 
+       var view = new google.visualization.DataView(data);
+       
        // Create a dashboard.
        var dashboard = new google.visualization.Dashboard(
          document.getElementById('dashboard_div'));
 
+
        // Create a range slider, passing some options
        var donutRangeSlider = new google.visualization.ControlWrapper({
-         'controlType': 'ChartRangeFilter',
+         'controlType': 'NumberRangeFilter',
          'containerId': 'filter_div',
          'options': {
-           'filterColumnLabel': 'Minutter',
-           'ui': {
-             chartType: 'LineChart',
-             snapToData: 'true',
-             minRangeSize: 5,
-             chartOptions: {
-               pointSize: 2,
-               series: {
-                 0: {
-                   color: 'GREEN'
-                 },
-                 1: {
-                   color: 'ORANGE'
-                 },
-                 2: {
-                   color: 'RED'
-                 }
-               }
-             }
-           }
-
-
+           'filterColumnLabel': 'Minutter'
          }
        });
 
@@ -60,37 +42,55 @@
          'chartType': 'ColumnChart',
          'containerId': 'chart_div',
          'options': {
-           animation: {
-             'startup': true,
-             'duration': 1000,
-             'easing': 'inAndOut'
-           },
-           annotation: {
-             'column_id': {
-               style: 'line'
-             }
-           },
-           vAxis: {
-             title: 'Antall'
-           },
-           hAxis: {
-             title: 'Minutter',
-             titleTextStyle: {
-               color: 'BLACK'
-             },
-             textPosition: 'out'
-           },
-           explorer: {
-             keepInBounds: true
-           },
-           'title': 'Resultat for event',
-           'subtitle': 'Se nærmere på tilbakemeldingene dine',
-           'width': 1240,
-           'height': 300,
-           'legend': 'right',
-           'colors': ['GREEN', 'ORANGE', 'RED']
+           colors: ['green', 'orange', 'red'],
+           'animation': {
+             "startup": true,
+             'duration': '1000',
+             'easing': 'out',
+           }
          }
        });
+
+       var bytt = false;
+       switchView = function() {
+         if (bytt === false) {
+           columnChart.setChartType('LineChart');
+           dashboard.draw(view);
+           bytt = true;
+         } else {
+           columnChart.setChartType('ColumnChart');
+           dashboard.draw(view);
+           bytt = false;
+         }
+       };
+
+       showAll = function() {
+         view.setColumns([0, 1, 2, 3]);
+         columnChart.setOptions({
+           colors: ['green', 'orange', 'red'],
+           'animation': {
+             "startup": true,
+             'duration': '1000',
+             'easing': 'out',
+           }
+         });
+         dashboard.draw(view);
+       }
+
+       showColumn = function(id, farge) {
+         view.setColumns([0, id]);
+         columnChart.setOptions({
+           colors: [farge],
+           'animation': {
+             "startup": true,
+             'duration': '1000',
+             'easing': 'out',
+           }
+         });
+         dashboard.draw(view);
+       }
+
+
 
        // Establish dependencies, declaring that 'filter' drives 'columnChart',
        // so that the column chart will only display entries that are let through
@@ -98,5 +98,9 @@
        dashboard.bind(donutRangeSlider, columnChart);
 
        // Draw the dashboard.
-       dashboard.draw(data);
+       if (view === null) {
+         dashboard.draw(data);
+       } else {
+         dashboard.draw(view);
+       }
      }
