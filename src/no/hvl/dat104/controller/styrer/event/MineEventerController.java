ba9d0ec0ab04ac7f336f72ebc19a9684263e9524
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-10-31 22:00:24 +0000

    MineEventerController + JSP view
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@156 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

---
+++ src/no/hvl/dat104/controller/styrer/event/MineEventerController.java
@@ -0,0 +1,53 @@
+package no.hvl.dat104.controller.styrer.event;
+
+import static no.hvl.dat104.controller.JspMappings.MINEAKTIVITETER_JSP;
+import static no.hvl.dat104.controller.JspMappings.MINEEVENTER_JSP;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import no.hvl.dat104.dataaccess.IAktivitetEAO;
+import no.hvl.dat104.model.Aktivitet;
+import no.hvl.dat104.model.Event;
+import no.hvl.dat104.util.ValidatorUtil;
+
+/**
+ * Servlet implementation class MineEventerController
+ */
+public class MineEventerController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private IAktivitetEAO aktivitetEAO;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String aktivitetsId = request.getParameter("aktivitetId");
+		if (!ValidatorUtil.isNotNull0(aktivitetsId)) {
+			request.getRequestDispatcher(MINEAKTIVITETER_JSP).forward(request, response);
+		} else {
+			int id = Integer.parseInt(aktivitetsId);
+
+			Aktivitet a = aktivitetEAO.finnAktivitet(id);
+			List<Event> eventer = aktivitetEAO.finnAlleEventerTilAktivitet(a.getId());
+
+			Collections.sort(eventer, new Comparator<Event>() {
+				public int compare(Event e1, Event e2) {
+					return e1.getTidFra().compareTo(e2.getTidFra());
+				}
+			});
+			request.getSession().setAttribute("eventer", eventer);
+			request.getRequestDispatcher(MINEEVENTER_JSP).forward(request, response);
+		}
+
+	}
+
+}
--- web/WEB-INF/views/styrer/event/mineeventer.jsp
+++ web/WEB-INF/views/styrer/event/mineeventer.jsp
@@ -1,4 +1,39 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page import="static no.hvl.dat104.controller.UrlMappings.*" %>
+<%@ page import="static no.hvl.dat104.model.Status.*" %>
 <jsp:include page="../../../partials/header.jsp" />
-	 <p>Mine eventer</p>
+	<c:set var="PAAGANDE" value="<%=PAAGANDE%>"></c:set>
+<c:set var="AVSLUTTET" value="<%=AVSLUTTET%>"></c:set>
+<div class="ui container">
+	<p>Mine eventer</p>
+	<table class="ui fixed single line celled table">
+	  <thead>
+	    <tr><th>Navn</th>
+	    <th>Status</th>
+	    <th>Rediger</th>
+	  </tr></thead>
+	  <tbody>	  
+	  	<c:forEach  items="${eventer}" var = "event">
+	        <tr>
+		      <td><c:out value = "${event.navn}"/></td>
+		      <td class="${event.status eq PAAGANDE ? 'positive' : (event.status eq AVSLUTTET) ? 'error' : 'warning'}"><c:out value = "${event.status}"/></td>
+		      <td><a href="<%= REDIGEREVENT_URL%>?aktivitetId=${event.id}">Rediger</a></td>
+	    	</tr>	  
+	     </c:forEach>
+	  </tbody>
+	</table>
+</div>
+	 <script type="text/javascript">
+	    var visForm = document.getElementById('vis');
+ 		var redigerForm = document.getElementById('rediger');
+ 		
+ 		visForm.addEventListener("click", function(){
+ 			document.getElementById("visForm").submit();
+ 		});
+ 		
+ 		redigerForm.addEventListener("click", function(){
+ 		    document.getElementById("redigerForm").submit();
+ 		});
+ 		
+	 </script>
 <jsp:include page="../../../partials/footer.jsp" />
