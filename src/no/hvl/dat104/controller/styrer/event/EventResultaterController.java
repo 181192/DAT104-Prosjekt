09779e722a2f489e227a95d43ef140275aaa004a
Fredrik <h148916@stud.hib.no>
2017-11-03 16:43:46 +0000

    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@173 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/controller/styrer/event/EventResultaterController.java
+++ src/no/hvl/dat104/controller/styrer/event/EventResultaterController.java
@@ -1,6 +1,7 @@
 package no.hvl.dat104.controller.styrer.event;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -10,7 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import no.hvl.dat104.controller.JspMappings;
 import no.hvl.dat104.dataaccess.IEventEAO;
+import no.hvl.dat104.dataaccess.ITilbakemeldingEAO;
 import no.hvl.dat104.model.Event;
+import no.hvl.dat104.model.Tilbakemelding;
+import no.hvl.dat104.util.FormaterTilbakemeldingUtil;
+import no.hvl.dat104.util.FormatertTilbakemelding;
 
 /**
  * Servlet implementation class EventResultaterController
@@ -19,13 +24,27 @@ public class EventResultaterController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@EJB
     private IEventEAO iEventEAO;
+	@EJB
+	private ITilbakemeldingEAO iTilbakemeldingEao;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		//Får tak i event id
 		Integer id = Integer.parseInt(request.getParameter("eventId"));
 		Event e = iEventEAO.finnEvent(id);
+		//Får tak i liste med tilbakemeldinger for eventet, deretter konverterer den til et format som kan brukes i grafene
+		List<Tilbakemelding> mellomLagretListe = iTilbakemeldingEao.alleTilbakemeldingene();
+		
+		//Må filtrere ut tilbakemeldingene...
+		List<FormatertTilbakemelding> formaterteTilbakemeldinger = FormaterTilbakemeldingUtil.formaterTilbakemeldinger(mellomLagretListe);
+		for(FormatertTilbakemelding t : formaterteTilbakemeldinger) {
+			System.out.println(t.getTid() + "  -  " + t.getFornoyd() +"   " + t.getNoytral() + "      " + t.getMisfornoyd());
+		}
+		//Attributter får verdiene sine tilsendt
 		request.setAttribute("aktivitet", e.getIdAktivitet());
 		request.setAttribute("event", e);
+		request.setAttribute("formaterteTilbakemeldinger", formaterteTilbakemeldinger);
 		request.getRequestDispatcher(JspMappings.EVENTRESULTATER_JSP).forward(request, response);//Midlertidlig graf -Fredrik
 	}
 
