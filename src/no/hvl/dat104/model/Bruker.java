0453713874aa4e8907973c8eb738eac93e060c09
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-11-06 15:10:34 +0000

    Fjernet ubrukt import
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@206 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/model/Bruker.java
+++ src/no/hvl/dat104/model/Bruker.java
@@ -2,7 +2,7 @@ package no.hvl.dat104.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.FetchType;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
