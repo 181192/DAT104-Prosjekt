fa9c285d94cb37929218280e982821af814dcf71
Liv Sarah <h182021@stud.hib.no>
2017-11-07 12:44:23 +0000

    Oppdatert brukerTest
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@232 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/junit/OpprettBrukerTest.java
+++ src/no/hvl/dat104/junit/OpprettBrukerTest.java
@@ -2,45 +2,44 @@ package no.hvl.dat104.junit;
 
 import static org.junit.Assert.*;
 
-import javax.ejb.EJB;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import no.hvl.dat104.controller.styrer.bruker.BrukerValidator;
-import no.hvl.dat104.dataaccess.IBrukerEAO;
-import no.hvl.dat104.dataaccess.IRolleEAO;
 import no.hvl.dat104.model.Bruker;
+import no.hvl.dat104.model.Rolle;
 
 public class OpprettBrukerTest {
-
-	@EJB
-	IRolleEAO rolleEAO;
-	@EJB
-	IBrukerEAO brukerEAO;
 	
 	private String fornavn = "Ola";
 	private String etternavn = "Dunk";
 	private String mail = "ola@gruppe3.no";
 	private String passord = "passord";
 	private BrukerValidator bv;
+	//Funker ikke å hente rolle fra DB
+	private Rolle rolle;
+	private Bruker b;
 	
 	@Before
 	public void setUp() throws Exception {
-		Bruker b = new Bruker();
+		rolle = new Rolle("Tester");
+		b = new Bruker();
 		b.setFornavn(fornavn);
 		b.setEtternavn(etternavn);
 		b.setMail(mail);
 		b.setPassord(passord);
-		b.setIdRolle(rolleEAO.finnRolle(2));
+		b.setIdRolle(rolle);
 		b.setSalt("salt");
 		bv = new BrukerValidator(b);
-		brukerEAO.leggTilBruker(b);
 	}
 	
 	@Test
 	public void test() {
 		assertTrue(bv.erAlleDataGyldig());
+		assertTrue(b.getFornavn().equals(fornavn));
+		assertTrue(b.getEtternavn().equals(etternavn));
+		assertTrue(b.getMail().equals(mail));
+		assertTrue(b.getPassord().equals(passord));
 	}
 
 }
