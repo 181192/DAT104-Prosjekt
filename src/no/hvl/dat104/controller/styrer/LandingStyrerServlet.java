d56af8259c384b523f742553ab78e16e9744643a
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-11-08 22:24:55 +0000

    Autorisering på LandingStyreServlet
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@278 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/controller/styrer/LandingStyrerServlet.java
+++ src/no/hvl/dat104/controller/styrer/LandingStyrerServlet.java
@@ -10,12 +10,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import no.hvl.dat104.controller.JspMappings;
+import no.hvl.dat104.controller.UrlMappings;
 import no.hvl.dat104.dataaccess.IAktivitetEAO;
 import no.hvl.dat104.dataaccess.IBrukerEAO;
 import no.hvl.dat104.dataaccess.IEventEAO;
 import no.hvl.dat104.model.Aktivitet;
 import no.hvl.dat104.model.Bruker;
 import no.hvl.dat104.model.Event;
+import no.hvl.dat104.util.InnloggingUtil;
 
 /**
  * Indexsiden til brukeren
@@ -36,28 +38,22 @@ public class LandingStyrerServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		Bruker bruker = brukerEAO.finnBruker(2);
-		List<Aktivitet> a = brukerEAO.finnAlleAktiviteterTilBruker(bruker.getId());
-		System.out.println(a);
-		for(Aktivitet ak : a) {
-			System.out.println(ak.getNavn());
+		if (InnloggingUtil.erInnloggetSomBruker(request)) {
+			Bruker bruker = brukerEAO.finnBruker(2);
+			List<Aktivitet> a = brukerEAO.finnAlleAktiviteterTilBruker(bruker.getId());
+			System.out.println(a);
+			for (Aktivitet ak : a) {
+				System.out.println(ak.getNavn());
+			}
+			List<Event> alleEventer = brukerEAO.finnAlleEventerTilBruker(bruker.getId());
+			String[] farger = { "orange", "green", "red", "orange", "yellow" };
+			request.getSession().setAttribute("alleEventer", alleEventer);
+			request.getSession().setAttribute("aktiviteter", a);
+			request.getSession().setAttribute("color", farger);
+			request.getRequestDispatcher(JspMappings.LANDING_STYRER_JSP).forward(request, response);
+		} else {
+			response.sendRedirect(UrlMappings.LOGGINN_URL);
 		}
-		List<Event> alleEventer = brukerEAO.finnAlleEventerTilBruker(bruker.getId());
-		String[] farger = { "orange", "green", "red", "orange", "yellow" };
-		request.getSession().setAttribute("alleEventer", alleEventer);
-		request.getSession().setAttribute("aktiviteter", a);
-		request.getSession().setAttribute("color", farger);
-		request.getRequestDispatcher(JspMappings.LANDING_STYRER_JSP).forward(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
