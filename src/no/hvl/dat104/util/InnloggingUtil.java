15b1aad964a2ef9e463653d020c758585245c034
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-11-08 19:23:46 +0000

    Endret navn, og endre currentUser til bruker
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@264 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/util/InnloggingUtil.java
+++ src/no/hvl/dat104/util/InnloggingUtil.java
@@ -3,122 +3,121 @@ package no.hvl.dat104.util;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import no.hvl.dat104.controller.Attributter;
 import no.hvl.dat104.model.Bruker;
 import no.hvl.dat104.model.Event;
 
 public class InnloggingUtil {
-	 /**
-     * Sjekker om bruker er innlogget
-     * @param request
-     * @return
-     */
-    public static boolean isInnlogget(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        return (session != null)
-                && (session.getAttribute("currentUser") != null);
-    }
-    /**
-     * Returner sann om bruker er innlogget
-     * @param request
-     * @return
-     */
-    public static Bruker isInnloggetSom(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        return isInnlogget(request) ?
-                (Bruker) session.getAttribute("currentUser") : null;
-    }
-    /**
-     * Logger inn som en deltager
-     * @param request
-     */
-    public static void loggInnSomDeltager(HttpServletRequest request, Event event) {
-    	loggUt(request);
-        HttpSession sesjon = request.getSession(true);
-        sesjon.setAttribute("event", event);
-    }
-    /**
-     * Logger inn som Bruker
-     * @param request
-     * @param b
-     * @param init henter fra init parameter i xml.
-     */
-    public static void loggInnSomBruker(HttpServletRequest request, Bruker b, String init) {
-        loggUt(request);
-        HttpSession sesjon = request.getSession(true);
-        sesjon.setMaxInactiveInterval(Integer.parseInt(init));
-        sesjon.setAttribute("bruker", b);
-    }
-  
-    /**
-     * Denne metoden vil være for en aktivitetsstyrer.
-     * Logger inn med Bruker objektet.
-     * Setter currentUser som brukeren
-     * @param request
-     * @param b
-     */
-    public static void loggInnSomBruker(HttpServletRequest request, Bruker b) {
-        loggUt(request);
-        HttpSession sesjon = request.getSession(true);
-        sesjon.setAttribute("bruker", b);
-        sesjon.setMaxInactiveInterval(1200);
-    }
-    /**
-     * Denne metoden vil være for en aktivitetsstyrer.
-     * Logger inn med brukernavn som streng
-     * @param request
-     * @param brukernavn
-     */
-    public static void loggInnSom(HttpServletRequest request, String brukernavn) {
-        loggUt(request);
-        HttpSession sesjon = request.getSession(true);
-        sesjon.setAttribute("currentUser", brukernavn);
-        sesjon.setMaxInactiveInterval(1200);
-    }
-    /**
-     * Sjekker sessionen er admin
-     * @param request
-     * @return
-     */
-    public static boolean isInnLoggetSomAdmin(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        return (session != null)
-                && (session.getAttribute("admin") != null);
-    }
-    /**
-     * Logger inn som admin
-     * @param request
-     */
-    public static void loggInnSomAdmin(HttpServletRequest request) {
-        loggUt(request);
-        HttpSession sesjon = request.getSession(true);
-        sesjon.setAttribute("admin", "admin");
-        sesjon.setMaxInactiveInterval(1200);
-    }
-    
-    /**
-     * logger deg ut!
-     * @param request
-     */
-    public static void loggUt(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
-    }
-    
-    /**
-     * Sjekker om deltager har skrivd inn kodeord / er innlogget
-     * @param request
-     * @return boolean
-     */
-    public static boolean erInnloggetSomDeltager(HttpServletRequest request) {
-        HttpSession sesjon = request.getSession(false);
-        if (sesjon!=null) {
-        	return sesjon.getAttribute("event")!=null;
-        }
-        else {
-        	return false;
-        }
-    }
+	/**
+	 * Sjekker om bruker er innlogget
+	 * 
+	 * @param request
+	 * @return True eller False
+	 */
+	public static boolean erInnloggetSomBruker(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		return (session != null) && (session.getAttribute(Attributter.BRUKER) != null);
+	}
+
+	/**
+	 * Returner brukeren som er innlogget, ellers null
+	 * 
+	 * @param request
+	 * @return Brukeren
+	 */
+	public static Bruker innloggetSomBruker(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		return erInnloggetSomBruker(request) ? (Bruker) session.getAttribute(Attributter.BRUKER) : null;
+	}
+
+	/**
+	 * Logger inn som Bruker
+	 * 
+	 * @param request
+	 * @param b
+	 * @param init
+	 *            henter fra init parameter i xml.
+	 */
+	public static void loggInnSomBruker(HttpServletRequest request, Bruker b, String init) {
+		loggUt(request);
+		HttpSession sesjon = request.getSession(true);
+		sesjon.setMaxInactiveInterval(Integer.parseInt(init));
+		sesjon.setAttribute(Attributter.BRUKER, b);
+	}
+
+	/**
+	 * Denne metoden vil være for en aktivitetsstyrer. Logger inn med Bruker
+	 * objektet. Setter bruker som brukeren
+	 * 
+	 * @param request
+	 * @param b Brukeren
+	 */
+	public static void loggInnSomBruker(HttpServletRequest request, Bruker b) {
+		loggUt(request);
+		HttpSession sesjon = request.getSession(true);
+		sesjon.setAttribute(Attributter.BRUKER, b);
+		sesjon.setMaxInactiveInterval(1200);
+	}
+
+	/**
+	 * Logger inn som admin
+	 * 
+	 * @param request
+	 */
+	public static void loggInnSomAdmin(HttpServletRequest request) {
+		loggUt(request);
+		HttpSession sesjon = request.getSession(true);
+		sesjon.setAttribute(Attributter.ADMIN, Attributter.ADMIN);
+		sesjon.setMaxInactiveInterval(1200);
+	}
+
+	/**
+	 * Sjekker sessionen er admin
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public static boolean erInnLoggetSomAdmin(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		return (session != null) && (session.getAttribute(Attributter.ADMIN) != null);
+	}
+
+	/**
+	 * Logger inn som en deltager
+	 * 
+	 * @param request
+	 */
+	public static void loggInnSomDeltager(HttpServletRequest request, Event event) {
+		loggUt(request);
+		HttpSession sesjon = request.getSession(true);
+		sesjon.setAttribute("event", event);
+	}
+
+	/**
+	 * Sjekker om deltager har skrivd inn kodeord / er innlogget
+	 * 
+	 * @param request
+	 * @return boolean
+	 */
+	public static boolean erInnloggetSomDeltager(HttpServletRequest request) {
+		HttpSession sesjon = request.getSession(false);
+		if (sesjon != null) {
+			return sesjon.getAttribute("event") != null;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * logger deg ut!
+	 * 
+	 * @param request
+	 */
+	public static void loggUt(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			session.invalidate();
+		}
+	}
 
 }
