8dccf3344a8980baa30cee3c3b1f512a0d49dc8d
Kathinka Neteland <h181210@stud.hib.no>
2017-11-08 16:46:27 +0000

    Added test om bruker er innlogget
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@250 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/controller/styrer/bruker/RedigerBrukerController.java
+++ src/no/hvl/dat104/controller/styrer/bruker/RedigerBrukerController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +13,7 @@ import no.hvl.dat104.controller.UrlMappings;
 import no.hvl.dat104.dataaccess.IBrukerEAO;
 import no.hvl.dat104.model.Bruker;
 import no.hvl.dat104.util.FlashUtil;
-import no.hvl.dat104.util.ValidatorUtil;
+import no.hvl.dat104.util.InnloggingUtil;
 
 /**
  * Servlet implementation class RedigerBrukerController
@@ -24,53 +23,59 @@ public class RedigerBrukerController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@EJB
 	private IBrukerEAO brukerEAO;
-	
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		//Bruker b = (Bruker) request.getSession().getAttribute("currentUser");
-		
-		Bruker b = brukerEAO.finnBruker(2);
-		
-		
-		System.out.println(b.getFornavn());
-		request.getSession().setAttribute("currentUser", b);
-		
-		request.getRequestDispatcher(JspMappings.REDIGERBRUKER_JSP).forward(request, response);
+
+	@SuppressWarnings("static-access")
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Bruker b = (Bruker) request.getSession().getAttribute("currentUser");
+
+		InnloggingUtil logIn = new InnloggingUtil();
+
+		if (logIn.isInnlogget(request)) {
+
+			if (b != null) {
+				request.getSession().setAttribute("currentUser", b);
+
+				request.getRequestDispatcher(JspMappings.REDIGERBRUKER_JSP).forward(request, response);
+			} else {
+
+				response.sendRedirect(UrlMappings.LANDING_STYRER_URL);
+			}
+
+		} else {
+			response.sendRedirect(UrlMappings.LOGGINN_URL);
+		}
+
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		BrukerValidator skjema = new BrukerValidator(request);
 		Bruker b = (Bruker) request.getSession().getAttribute("currentUser");
-		
-		
-		
+
 		if (skjema.erAlleDataGyldig()) {
 			if (b != null) {
 				Integer id = b.getId();
-				
+
 				brukerEAO.endreEtternavnPaaBruker(id, skjema.getEtternavn());
 				brukerEAO.endreFornavnPaaBruker(id, skjema.getFornavn());
 				brukerEAO.endreMailPaaBruker(id, skjema.getMail());
 				brukerEAO.endrePassordPaaBruker(id, skjema.getPassord());
-			
+
 				FlashUtil.Flash(request, "success", "Brukeren " + b.getFornavn() + b.getEtternavn() + " er oppdatert!");
 				response.sendRedirect(UrlMappings.LANDING_STYRER_URL);
-			}  else {
+			} else {
 				FlashUtil.Flash(request, "error", "Beklager, noe gikk galt");
 				response.sendRedirect(UrlMappings.REDIGERBRUKER_URL);
 			}
 		} else {
 			skjema.settOppFeilmeldinger(request);
 			response.sendRedirect(UrlMappings.REDIGERBRUKER_URL);
-			
+
 		}
-		
+
 	}
 
 }
