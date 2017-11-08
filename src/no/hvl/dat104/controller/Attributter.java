3500370184f0e8f0f39e2a0079583cc1a5484872
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-11-08 19:23:22 +0000

    Lagt til konstanter for bruker,admin og deltager til bruk i sesjoner
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@263 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/controller/Attributter.java
+++ src/no/hvl/dat104/controller/Attributter.java
@@ -4,4 +4,7 @@ public final class Attributter {
 	public static final String LIVE_EVENT = "liveevent";
 	public static final String KODEORD = "kodeord";
 	public static final String LIVE_EVENT_KNAPP = "liveeventknapp";
+	public static final String BRUKER = "bruker";
+	public static final String ADMIN = "admin";
+	public static final String DELTAGER = "deltager";
 }
