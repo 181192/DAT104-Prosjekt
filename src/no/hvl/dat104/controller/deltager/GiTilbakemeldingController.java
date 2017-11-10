08d0abeecd80f4edcec65608b8b29a7e15b8a72a
Fredrik <h148916@stud.hib.no>
2017-11-10 17:43:46 +0000

    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@367 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/controller/deltager/GiTilbakemeldingController.java
+++ src/no/hvl/dat104/controller/deltager/GiTilbakemeldingController.java
@@ -1,6 +1,7 @@
 package no.hvl.dat104.controller.deltager;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -11,8 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 import no.hvl.dat104.controller.JspMappings;
 import no.hvl.dat104.controller.UrlMappings;
 import no.hvl.dat104.dataaccess.IEventEAO;
-import no.hvl.dat104.dataaccess.ILiveTilbakemeldingEAO;
 import no.hvl.dat104.dataaccess.ITilbakemeldingEAO;
+import no.hvl.dat104.dataaccess.ILiveTilbakemeldingEAO;
+import no.hvl.dat104.model.Event;
+import no.hvl.dat104.model.Tilbakemelding;
+import no.hvl.dat104.model.LiveTilbakemelding;
+import no.hvl.dat104.util.DatoUtil;
+import no.hvl.dat104.util.FlashUtil;
 import no.hvl.dat104.util.InnloggingUtil;
 
 /**
@@ -21,16 +27,16 @@ import no.hvl.dat104.util.InnloggingUtil;
 public class GiTilbakemeldingController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     @EJB
-    private ITilbakemeldingEAO tilbakemeldingEAO;
+    private ITilbakemeldingEAO iTilbakemeldingEAO;
     @EJB
-    private IEventEAO eventEAO;
+    private IEventEAO iEventEAO;
     @EJB
-    private ILiveTilbakemeldingEAO liveTilbakemeldingEAO; 
+    private ILiveTilbakemeldingEAO ILiveTilbakemeldingEAO; 
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		if (InnloggingUtil.erInnloggetSomDeltager(request) && !GiTilbakemeldingHjelpeklasse.avgittStemmeTidligere(request)) {
+		if (InnloggingUtil.erInnloggetSomDeltager(request)) {
 			request.getRequestDispatcher(JspMappings.GITILBAKEMELDING_JSP).forward(request, response);
 		}
 		else {
@@ -41,12 +47,67 @@ public class GiTilbakemeldingController extends HttpServlet {
     @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-    	if (GiTilbakemeldingHjelpeklasse.eventHarStartet(request, eventEAO) && !GiTilbakemeldingHjelpeklasse.maaVente(request, 10)) {
-        	GiTilbakemeldingHjelpeklasse.lastOppTilbakemelding(request, liveTilbakemeldingEAO, tilbakemeldingEAO, response, eventEAO);
+		System.out.println("doPost metoden");
+    	if (eventHarStartet(request) && !maaVente(request, 10)) {
+    		System.out.println("event har startet");
+    		if(eventPaagaar(request)) {
+        		System.out.println("event paagaar");
+        		lastOppTilbakemelding(request,"paagaaende");
+        		FlashUtil.Flash(request, "Success", "Sist stemme avgitt " + new Date().toString().substring(11, 19));
+        		response.sendRedirect("gitilbakemelding");
+    		}
+    		else {//helhetlig tilbakemelding
+        		System.out.println("event avsluttet");
+        		lastOppTilbakemelding(request,"heletlig");
+        		FlashUtil.Flash(request, "Success", "Sist stemme avgitt " + new Date().toString().substring(11, 19));
+        		response.sendRedirect("gitilbakemelding");
+    		}
     	}
     	else {
-    		GiTilbakemeldingHjelpeklasse.settFeilmelding(request);
+    		System.out.println("event har ikke startet");
+			FlashUtil.Flash(request, "Error", "Eventet har ikke startet");
     		response.sendRedirect("gitilbakemelding");
     	}
+	}
+    
+    private boolean maaVente(HttpServletRequest request, int sekunder) {
+    	Date avgittDato = (Date) request.getAttribute("sistStemme");
+    	if (request.getAttribute("sistStemme")!=null) {
+    		Date naa = new Date();
+    		long sekunderMellom = (naa.getTime() - avgittDato.getTime() / 1000);
+    		return sekunderMellom >= sekunder;
+    		
+    	}
+    	else {
+    		return false;
+    	}
+    }
+
+    
+    private boolean eventPaagaar(HttpServletRequest request) {
+    	Event event = (Event) request.getSession().getAttribute("event");
+    	return event.getTidTil().after(new Date()); 
+    }
+    
+    private boolean eventHarStartet(HttpServletRequest request) {
+    	Event event = (Event) request.getSession().getAttribute("event");
+		System.out.println("Tid fra: " + event.getTidFra());
+    	return new Date().after(event.getTidFra()); 
+    }
+
+	private void lastOppTilbakemelding(HttpServletRequest request, String type) {
+		if (type.equals("paagaaende")) {
+			System.out.println("Par 1: " + request.getParameter("tilbakemelding") + ", par 2: " + DatoUtil.lagCurrentTimestamp() + ", par 3: " + (request.getSession(true).getAttribute("event")!=null));
+			LiveTilbakemelding liveTilbakemelding = new LiveTilbakemelding(request.getParameter("tilbakemelding"),
+					DatoUtil.lagCurrentTimestamp(), (Event) request.getSession(true).getAttribute("event"));
+			System.out.println(liveTilbakemelding != null);
+			//denne kaster nullpointexception
+			ILiveTilbakemeldingEAO.leggTilLiveTilbakemelding(liveTilbakemelding);
+		}
+		else {//helehtlig
+			Tilbakemelding tilbakemelding = new Tilbakemelding(request.getParameter("tilbakemelding"),
+					(Event) request.getSession(true).getAttribute("event"), DatoUtil.lagCurrentTimestamp());
+			iTilbakemeldingEAO.leggTilTilbakemelding(tilbakemelding);
+		}
 	}
 }
