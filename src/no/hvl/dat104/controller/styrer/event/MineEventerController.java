ab8ca282eba7a5a14a489e3c423f8de84004ebd7
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-11-10 14:28:12 +0000

    Cleanup
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@352 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/controller/styrer/event/MineEventerController.java
+++ src/no/hvl/dat104/controller/styrer/event/MineEventerController.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.ejb.EJB;
-import javax.faces.context.Flash;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
