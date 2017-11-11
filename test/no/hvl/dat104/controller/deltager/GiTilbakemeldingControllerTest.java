468d36a332f6f00b84435f9bf801253101a0ab05
Karl <h181207@stud.hib.no>
2017-11-11 15:55:28 +0000

    testings
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@381 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- test/no/hvl/dat104/controller/deltager/GiTilbakemeldingControllerTest.java
+++ test/no/hvl/dat104/controller/deltager/GiTilbakemeldingControllerTest.java
@@ -9,7 +9,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import no.hvl.dat104.controller.JspMappings;
+import no.hvl.dat104.controller.UrlMappings;
 import no.hvl.dat104.controller.deltager.GiTilbakemeldingController;
+import no.hvl.dat104.model.Event;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
@@ -23,6 +26,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -30,10 +34,11 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class GiTilbakemeldingControllerTest {
 	@Mock HttpServletRequest stubRequest;
 	@Mock HttpServletResponse stubResponse;
+	@Mock HttpSession stubSesjon;
 	@Mock RequestDispatcher stubRequestDispatcher;
     GiTilbakemeldingController giTilbakemeldingController;
     
@@ -41,17 +46,31 @@ public class GiTilbakemeldingControllerTest {
 	public void setUp() {
         giTilbakemeldingController = new GiTilbakemeldingController();
         
+        when(stubRequest.getSession(any(boolean.class))).thenReturn(stubSesjon);
         when(stubRequest.getRequestDispatcher(any(String.class))).thenReturn(stubRequestDispatcher);
 	}
 
     @Test
     public void atDetGjoresEnForwardTilKorrektJsp() throws ServletException, IOException {
+    	stubSesjon.setAttribute("event", new Event());
         
     	giTilbakemeldingController.doGet(stubRequest, stubResponse);
-        
-        verify(stubRequest).getRequestDispatcher("WEB-INF/views/deltager/gitilbakemelding.jsp");
-        verify(stubRequestDispatcher).forward(stubRequest, stubResponse);
+    	
+    	//verify(stubRequest).getSession(false);
+    	//verify(stubSession).getAttribute("event");
+    	//verify(stubResponse).sendRedirect(UrlMappings.DELTAEVENT_URL);
+        //verify(stubRequestDispatcher).forward(stubRequest, stubResponse);
+    	verify(stubSesjon).getAttribute("event");
+        //verify(stubRequest).getRequestDispatcher(JspMappings.GITILBAKEMELDING_JSP);
     }
+    
+    @Test
+    public void blirRedirectaTilDeltaEventVedManglendeData() throws ServletException, IOException {
+    	giTilbakemeldingController.doGet(stubRequest, stubResponse);
+    	
+    	verify(stubResponse).sendRedirect(UrlMappings.DELTAEVENT_URL);
+    }
+
 
 
 }
