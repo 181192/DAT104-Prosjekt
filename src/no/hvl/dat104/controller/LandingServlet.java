aba95e3df55f0128708f92da67f4b2bf405053f8
Peder Wiig <peder.wiig@gmail.com>
2017-11-12 14:23:20 +0000

    Om bruker er innlogget blir han redirected til loginn om han prøve å delta event
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@401 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/controller/LandingServlet.java
+++ src/no/hvl/dat104/controller/LandingServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import no.hvl.dat104.util.InnloggingUtil;
+
 /**
  * Servlet implementation class LandingServlet
  */
@@ -21,7 +23,12 @@ public class LandingServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		request.getRequestDispatcher(JspMappings.LANDING_JSP).forward(request, response);
+		if(InnloggingUtil.erInnloggetSomBruker(request) ||InnloggingUtil.erInnloggetSomAdmin((request))) {
+			response.sendRedirect(UrlMappings.LOGGINN_URL);
+		}else {
+			request.getRequestDispatcher(JspMappings.LANDING_JSP).forward(request, response);
+		}
+		
 	}
 
 	
