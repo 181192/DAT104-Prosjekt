758ca6baa3b836453f56d83dcce939d3316ac769
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-11-16 21:14:29 +0000

    Cleanup
    git-svn-id: http://data1.hib.no:8000/svn/h17_dat104_p03/gruppe3@449 7c32c8f6-663d-7b4d-90ba-b3f7713ca579

--- src/no/hvl/dat104/controller/styrer/event/PostLiveEventController.java
+++ src/no/hvl/dat104/controller/styrer/event/PostLiveEventController.java
@@ -14,10 +14,7 @@ import no.hvl.dat104.controller.Attributter;
 import no.hvl.dat104.controller.JspMappings;
 import no.hvl.dat104.controller.UrlMappings;
 import no.hvl.dat104.dataaccess.IEventEAO;
-import no.hvl.dat104.dataaccess.jpa.EventEAO;
 import no.hvl.dat104.model.Event;
-import no.hvl.dat104.model.Kodeord;
-import no.hvl.dat104.model.Status;
 import no.hvl.dat104.model.Tilbakemelding;
 import no.hvl.dat104.util.FormaterTilbakemeldingUtil;
 import no.hvl.dat104.util.InnloggingUtil;
@@ -27,48 +24,44 @@ import no.hvl.dat104.util.InnloggingUtil;
  */
 public class PostLiveEventController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	@EJB
 	IEventEAO eventEAO;
-	
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		response.setIntHeader("Refresh", 5);
-		
+
 		if (InnloggingUtil.erInnloggetSomBruker(request)) {
 			HttpSession session = request.getSession(false);
-			Kodeord koden = (Kodeord)session.getAttribute("koden");
-			System.out.println("koden: " + koden.getKode());
-			Event detteEvent= (Event)session.getAttribute(Attributter.LIVE_EVENT);
-			
+			Event detteEvent = (Event) session.getAttribute(Attributter.LIVE_EVENT);
+
 			Integer eventId = detteEvent.getId();
-			
+
 			List<Tilbakemelding> endeligTB = eventEAO.finnAlleTilbakemeldingerTilEvent(eventId);
-			
-			
+
 			Integer[] formaterteTB = new Integer[3];
 
 			formaterteTB = FormaterTilbakemeldingUtil.frekventTilbakemeldinger(endeligTB);
-			
+
 			request.setAttribute(Attributter.TILBAKEMELDINGS_ARRAY, formaterteTB);
-			
+
 			request.getRequestDispatcher(JspMappings.POST_LIVE_EVENT_JSP).forward(request, response);
 		} else {
 			response.sendRedirect(UrlMappings.LOGGINN_URL);
 		}
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		if (InnloggingUtil.erInnloggetSomBruker(request)) {
 			HttpSession session = request.getSession(false);
-			
+
 			Event denneEvent = (Event) session.getAttribute(Attributter.LIVE_EVENT);
-			
+
 			eventEAO.endreEventTilAvsluttet(denneEvent.getId());
-			
+
 			session.removeAttribute(Attributter.LIVE_EVENT);
 			response.sendRedirect(UrlMappings.LANDING_STYRER_URL);
 
@@ -76,6 +69,5 @@ public class PostLiveEventController extends HttpServlet {
 			response.sendRedirect(UrlMappings.LOGGINN_URL);
 		}
 	}
-	
-	
+
 }
